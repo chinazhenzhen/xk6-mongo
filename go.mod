df1285d9c5ba143d5b729d9a4cb39075914d5a77
Shiro Tian <xiaoqi.tian@iherb.com>
2022-03-08 16:52:51 +0800

    * Change source

--- go.mod
+++ go.mod
@@ -4,11 +4,16 @@ go 1.15
 
 require (
 	github.com/aws/aws-sdk-go v1.35.28 // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/gobuffalo/genny v0.1.1 // indirect
+	github.com/gobuffalo/gogen v0.1.1 // indirect
 	github.com/golang/snappy v0.0.2 // indirect
-	github.com/klauspost/compress v1.11.3 // indirect
+	github.com/karrick/godirwalk v1.10.3 // indirect
 	github.com/loadimpact/k6 v0.31.1
-	github.com/sirupsen/logrus v1.7.0 // indirect
+	github.com/pelletier/go-toml v1.7.0 // indirect
 	github.com/spf13/afero v1.4.1 // indirect
-	go.mongodb.org/mongo-driver v1.4.3
-	golang.org/x/text v0.3.4 // indirect
+	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
+	github.com/xdg/stringprep v0.0.0-20180714160509-73f8eece6fdc // indirect
+	go.k6.io/k6 v0.36.0
+	go.mongodb.org/mongo-driver v1.8.4
 )
--- mongo.go
+++ mongo.go
@@ -7,14 +7,14 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 
-	"github.com/loadimpact/k6/js/common"
-	"github.com/loadimpact/k6/js/modules"
+	k6common "go.k6.io/k6/js/common"
+	k6modules "go.k6.io/k6/js/modules"
 )
 
 // Register the extension on module initialization, available to
 // import from JS as "k6/x/mongo".
 func init() {
-	modules.Register("k6/x/mongo", new(Mongo))
+	k6modules.Register("k6/x/mongo", new(Mongo))
 }
 
 // Mongo is the k6 extension for a Mongo client.
@@ -29,13 +29,13 @@ type Client struct {
 // returns a new Mongo client object.
 // connURI -> mongodb://username:password@address:port/db?connect=direct
 func (m *Mongo) XClient(ctxPtr *context.Context, connURI string) interface{} {
-	rt := common.GetRuntime(*ctxPtr)
+	rt := k6common.GetRuntime(*ctxPtr)
 	clientOptions := options.Client().ApplyURI(connURI)
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
 		return err
 	}
-	return common.Bind(rt, &Client{client: client}, ctxPtr)
+	return k6common.Bind(rt, &Client{client: client}, ctxPtr)
 }
 
 func (c *Client) Insert(database string, collection string, doc map[string]string) error {
